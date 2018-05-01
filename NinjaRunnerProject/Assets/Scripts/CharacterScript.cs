a630876fde028482279b2f6471fd831748c4ce8f
jnastri <38079424+jnastri@users.noreply.github.com>
2018-05-01 02:37:20 -0400

    Player Controller Script
    Player controller script has been started. The character can jump, wall jump, move, move up hill, and move down hill. We will still need to adjust the input of the movement so it remains auto rather than input related.

---
+++ NinjaRunnerProject/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterScript : MonoBehaviour
+{
+    public float speed = 3;
+    public float jumpForce = 3;
+    public Transform groundPoint;
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Movement();
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+	}
+
+    void Movement()
+    {
+        if(gameObject.transform.localScale.z == 1)
+        {
+            gameObject.transform.Translate(1 * speed * Time.deltaTime, 0, 0);
+        }
+        else
+        {
+            gameObject.transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+        }
+    }
+
+    void Jump()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(groundPoint.transform.position, Vector2.down, 1);
+        Debug.DrawRay(groundPoint.transform.position, Vector2.down * 1, Color.red);
+        if (hit.collider.gameObject.layer == 8)
+        {
+            //Physics based jump
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ NinjaRunnerProject/Assets/Scripts/Player.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Controller2D))]
+public class Player : MonoBehaviour
+{
+    public float jumpHeight = 4;
+    public float timeToJumpApex = .4f;
+    public float accelerationTimeAirborne = .2f;
+    public float accelerationTimeGrounded = .1f;
+
+    public float moveSpeed = 5;
+
+    public float wallSlideSpeedMax = 3;
+    public Vector2 wallJump;
+
+    Vector3 velocity;
+    float velocityXSmoothing;
+
+    float jumpVelocity;
+    float gravity;
+
+    Controller2D playerController;
+    // Use this for initialization
+    void Start ()
+    {
+        playerController = GetComponent<Controller2D>();
+
+        gravity = -(2 * jumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        jumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        int wallDirX = (playerController.collisions.left) ? -1 : 1;
+
+        bool wallSliding = false;
+        if((playerController.collisions.left || playerController.collisions.right) && !playerController.collisions.below && velocity.y < 0)
+        {
+            wallSliding = true;
+            if(velocity.y < -wallSlideSpeedMax)
+            {
+                velocity.y = -wallSlideSpeedMax;
+            }
+        }
+
+        if(playerController.collisions.above || playerController.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            if (wallSliding)
+            {
+                if(input.x == 0)
+                {
+                    velocity.x = -wallDirX * wallJump.x;
+                    velocity.y = wallJump.y;
+                }
+            }
+            if (playerController.collisions.below)
+            {
+                velocity.y = jumpVelocity;
+            }
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (playerController.collisions.below)?accelerationTimeGrounded:accelerationTimeAirborne);
+        velocity.y += gravity * Time.deltaTime;
+        //playerController.AutoMove();
+        playerController.Move(velocity * Time.deltaTime);
+	}
+}
