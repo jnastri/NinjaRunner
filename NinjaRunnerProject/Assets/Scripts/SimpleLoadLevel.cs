b4d6a01e7ee8ab13ad408afe3df84f601120dc4e
JRamirez <38079767+Voltate@users.noreply.github.com>
2018-05-18 10:35:43 -0400

    Submitting Fading Script and Level Changer

---
+++ NinjaRunnerProject/Assets/Scripts/SimpleLoadLevel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SimpleLoadLevel : MonoBehaviour {
+
+    public bool changeLevel;
+    public string levelName;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+
+            StartCoroutine(ChangeLevel());
+        }
+    }
+
+    IEnumerator ChangeLevel()
+    {
+
+            float fadeTime = GameObject.Find("SceneManagement").GetComponent<Fading>().BeginFade(1);
+            yield return new WaitForSeconds(fadeTime);
+            Debug.Log("Changing Scenes");
+            SceneManager.LoadScene(levelName);
+        
+    }
+}
