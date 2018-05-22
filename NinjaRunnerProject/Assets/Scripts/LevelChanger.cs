ec4b962359750f97f8e049dc47df1a97ee1afb74
JRamirez <38079767+Voltate@users.noreply.github.com>
2018-05-22 13:56:21 -0400

    Adding New Level Changer and Testing Script

---
+++ NinjaRunnerProject/Assets/Scripts/LevelChanger.cs
@@ -0,0 +1,31 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelChanger : MonoBehaviour {
+
+    public Animator animator;
+    public string loadLevel;
+
+    public void Start()
+    {
+        //FadeToLevel(loadLevel);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+	}
+
+
+    public void FadeToLevel(string levelName)
+    {
+        loadLevel = levelName;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(loadLevel);
+    }
+}
---
+++ NinjaRunnerProject/Assets/Scripts/TestingLevelChanger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestingLevelChanger : MonoBehaviour {
+
+    public GameObject levelChanger;
+    [SerializeField]
+    string levelName;
+
+	// Use this for initialization
+	void Start () {
+
+        levelChanger = GameObject.Find("LevelChanger");
+
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonUp(0))
+        {
+            levelChanger.GetComponent<LevelChanger>().FadeToLevel(levelName);
+        }
+    }
+}
